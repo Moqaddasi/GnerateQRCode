ef631183ae581291876a5eaf0ec1fffc53f8ba71
Amirhosein Moqaddasi <ah.moqaddasi@gmail.com>
2022-11-25 02:01:58 +0330

    FEATURE:Implement first sample of QR code generator

---
+++ src/script.js
@@ -0,0 +1,11 @@
+
+
+function getUrl() {
+
+    const url = document.getElementById("url").value
+    fetch(`http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=300x300`)
+        .then(response => document.getElementById('qrCodeImage').src = response.url)
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    document.getElementById("yourLink").innerHTML = url
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      colors: {
+        primary: '#002333',
+        secondary: '#DEEFE7',
+        tertiary: '#159A9C',
+      },
+    },
+  },
+  plugins: [],
+}
