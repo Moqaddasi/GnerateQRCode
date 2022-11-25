b0342b2536951a259656113d98fb3d50a90cb2d3
Amirhosein Moqaddasi <ah.moqaddasi@gmail.com>
2022-11-26 02:04:16 +0330

    REFACTOR:Change styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        lg: '3rem',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
 
       colors: {
