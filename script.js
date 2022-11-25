e325b94c3108acb5c448c13678d44c3f0a9bd9b8
Amirhosein Moqaddasi <ah.moqaddasi@gmail.com>
2022-11-25 14:10:53 +0330

    DEBUG:Change Api request url

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function getUrl() {
 
     const url = document.getElementById("url").value
-    fetch(`http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=300x300`)
+    fetch(`https://api.qrserver.com/v1/create-qr-code/?data=${url}&size=300x300`)
         .then(response => document.getElementById('qrCodeImage').src = response.url)
         .then(response => console.log(response))
         .catch(err => console.error(err));
